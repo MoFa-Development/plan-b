6bdeebb40f4d0fd538aa833b7a2c5b88e8025d77
Antricks <33262801+Antricks@users.noreply.github.com>
2021-07-06 21:28:16 +0200

    add messages of the day, move to stylable format

--- js/main.js
+++ js/main.js
@@ -5,12 +5,17 @@ async function getData(dateOffset = currentDateOffset) {
 }
 
 function drawSubstitutionTable() {
-    var main = document.getElementById("main");
+    var messages = document.getElementById("messages");
+    var substitutions = document.getElementById("substitutions");
     var day_title = document.getElementById("title-day");
 
-    main.childNodes.forEach(child => {
-        main.removeChild(child)
-    })
+    while (substitutions.firstChild) {
+        substitutions.removeChild(substitutions.firstChild);
+    }
+
+    while (messages.firstChild) {
+        messages.removeChild(messages.firstChild);
+    }
 
     getData(currentDateOffset).then(data => {
         
@@ -20,6 +25,25 @@ function drawSubstitutionTable() {
         date_string = day + "." + month + "." + year;
 
         day_title.innerHTML = data.payload.weekDay + ", " + date_string;
+
+        data.payload.messageData.messages.forEach(messageData => {
+            let messageElement = document.createElement("div");
+            messageElement.classList.add("message")
+
+            if(messageData.subject) {
+                let subjectElement = document.createElement("div");
+                subjectElement.innerHTML = messageData.subject;
+                subjectElement.id = "subject";
+                messageElement.appendChild(subjectElement);
+            }
+
+            let messageBodyElement = document.createElement("div");
+            messageBodyElement.innerHTML = messageData.body;
+            messageBodyElement.id = "message-body";
+            messageElement.appendChild(messageBodyElement);
+
+            messages.appendChild(messageElement);
+        })
 
         data.payload.rows.forEach(element => {
 
@@ -31,12 +55,51 @@ function drawSubstitutionTable() {
             let teacher     = element.data[5];
             let message     = element.data[6];
 
-            let cssClasses  = element.cssClasses.join(", ");
-            let group       = element.group; 
+            let cssClasses  = element.cssClasses;
+            let group       = element.group;
 
-            var p = document.createElement("p");
-            p.appendChild(document.createTextNode(`[${classes.join(", ")}] Stunden: ${periods} (${course}) ${room} ${teacher} --- ${message}`));
-            main.appendChild(p);
+            let substElement = document.createElement("div");
+            //p.appendChild(document.createTextNode(`[${classes.join(", ")}] Stunden: ${periods} (${course}) ${room} ${teacher} --- ${message}`));
+            
+            //substElement.innerHTML = `[${classes.join(", ")}] Stunden: ${periods} (${course}) ${room} ${teacher} <br> ${message} <hr>`;
+            
+            let periodsElement = document.createElement("p");
+            periodsElement.innerHTML = periods;
+            periodsElement.id = "periods";
+            substElement.appendChild(periodsElement);
+            
+            let classesElement = document.createElement("p");
+            classesElement.innerHTML = classes.join(", ");
+            classesElement.id = "classes";
+            substElement.appendChild(classesElement);
+            
+            let courseElement = document.createElement("p");
+            courseElement.innerHTML = course;
+            courseElement.id = "course"
+            substElement.appendChild(courseElement);
+            
+            let roomElement = document.createElement("p");
+            roomElement.innerHTML = room;
+            roomElement.id = "room"
+            substElement.appendChild(roomElement);
+            
+            let teacherElement = document.createElement("p");
+            teacherElement.innerHTML = teacher;
+            teacherElement.id = "teacher"
+            substElement.appendChild(teacherElement);
+            
+            if(message) {
+                let messageElement = document.createElement("p");
+                messageElement.innerHTML = message;
+                messageElement.id = "message";
+                substElement.appendChild(messageElement);
+            }
+            
+            substElement.classList.add("subst-element");
+            cssClasses.forEach(cssClass => substElement.classList.add(cssClass));
+            
+            substitutions.appendChild(substElement);
+            substitutions.appendChild(document.createElement("hr"))
         });
 
     });
