4b0d9e2a064dd1747df47c7c757613caa6dd5603
Antricks <33262801+Antricks@users.noreply.github.com>
2021-07-06 15:21:09 +0200

    basic data iteration

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-var SCHOOL = "Oekumenisches-Gymnasium"
+async function getData(dateOffset = 0) {
+    const data = await fetch("/php/getData.php?dateOffset="+dateOffset).then(response => response.json());
+    return data
+}
 
-function getData() {
-    req = new XMLHttpRequest()
+window.onload = function () {
+    getData().then(data => {
+        data.payload.rows.forEach(element => {
+            
+            let time        = element.data[0]
+            let classes     = element.data[1].split(", ")
+            let course_long = element.data[2]
+            let course      = element.data[2]
+            let room        = element.data[3]
+            let teacher     = element.data[4]
+            let subst_type  = element.data[5]
+            
+            let message     = element.data[6]
 
-    req.open("GET", "https://ajax.webuntis.com/WebUntis/monitor/substitution/data?school=" + SCHOOL, true);
+            let cssClasses  = element.cssClasses
+            let group       = element.group; 
 
-    req.onload = function() {
-        console.log(this)
-    };
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(JSON.stringify(element)));
+            document.getElementById("main").appendChild(p);
+        });
 
-    req.send();
+    })
 }
---
+++ php/getData.php
@@ -0,0 +1,83 @@
+<?php
+
+header("Content-type:application/json");
+
+$api_schoolName = "Oekumenisches-Gymnasium";
+
+$api_date = date("Ymd");
+
+$api_dateOffset = 0;
+if(isset($_GET["dateOffset"])) {
+    $api_dateOffset = $_GET["dateOffset"];
+
+    if (filter_var($api_dateOffset, FILTER_VALIDATE_INT) === false ) {
+        $api_dateOffset = 0;
+    }
+}
+
+$api_URL = "https://ajax.webuntis.com/WebUntis/monitor/substitution/data?school=$api_schoolName";
+
+$api_json = "{
+	\"formatName\": \"Vertretungsplan Flur\",
+	\"schoolName\": \"$api_schoolName\",
+	\"date\": $api_date,
+	\"dateOffset\": $api_dateOffset,
+	\"strikethrough\": false,
+	\"mergeBlocks\": true,
+	\"showOnlyFutureSub\": false,
+	\"showBreakSupervisions\": true,
+	\"showTeacher\": true,
+	\"showClass\": true,
+	\"showHour\": true,
+	\"showInfo\": true,
+	\"showRoom\": true,
+	\"showSubject\": true,
+	\"groupBy\": 1,
+	\"hideAbsent\": false,
+	\"departmentIds\": [],
+	\"departmentElementType\": -1,
+	\"hideCancelWithSubstitution\": true,
+	\"hideCancelCausedByEvent\": false,
+	\"showTime\": false,
+	\"showSubstText\": true,
+	\"showAbsentElements\": [
+		1
+	],
+	\"showAffectedElements\": [
+		1
+	],
+	\"showUnitTime\": true,
+	\"showMessages\": true,
+	\"showStudentgroup\": true,
+	\"enableSubstitutionFrom\": false,
+	\"showSubstitutionFrom\": 1615,
+	\"showTeacherOnEvent\": true,
+	\"showAbsentTeacher\": true,
+	\"strikethroughAbsentTeacher\": false,
+	\"activityTypeIds\": [
+		2,
+		3
+	],
+	\"showEvent\": true,
+	\"showCancel\": true,
+	\"showOnlyCancel\": false,
+	\"showSubstTypeColor\": true,
+	\"showExamSupervision\": true,
+	\"showUnheraldedExams\": true
+}";
+
+$context = stream_context_create(
+    array(
+        'http' => array(
+            'method'  => 'POST', // Request Method
+            'header'  => 'Content-type: application/json', // Request Headers
+            'content' => $api_json // Request Content
+        )
+    )
+);
+
+$contents = file_get_contents($api_URL, false, $context);
+
+echo $contents;
+
+?>
