b56bf2c0db51b9c7163defab241cbb5d672d1d30
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2021-09-10 14:20:37 +0200

    Bind cookies to url path

--- js/utils.js
+++ js/utils.js
@@ -14,7 +14,7 @@ window.setCookie = function(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Lax;";
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=" + window.location.pathname + ";SameSite=Lax;";
 }
 
 /**
