c176041918b7c5e0e632f17987a61ad14007c458
Spyrex <malikfk100@gmail.com>
2022-05-08 18:23:28 +0200

    Fix color settings and saving, removed legacy code

--- js/settings.js
+++ js/settings.js
@@ -76,16 +76,11 @@ window.apply_settings = function() {
     // Akzentfarbe
     Settings.add_setting("appearance", "accent-color", "settings-colors", "Akzentfarbe", (saved) => {
         if (saved === undefined) {
-            return  COLORS[0]
+            return 0
         }
         return saved
     }, (val) => {
-        const accentColor = val
-        const accentDark = window.shadeColor(val, -20)
-    
-        document.querySelector('body').style.setProperty('--accent', accentColor)
-        document.querySelector('body').style.setProperty('--accent-dark', accentDark)
-
+        // Update logic within component
         Settings.save()
     })
 
@@ -136,27 +131,21 @@ window.Settings = {
     save: function() {
 
         for(var category of Object.keys(Settings.categories)) {
-            
-            if(Settings.categories[category] === undefined)
-                continue
-            
-            if (!Settings.categories[category].settings)
-                Settings.categories[category].settings = {}
 
-            var settings = Settings.categories[category].settings
             for(var child of Settings.categories[category].childNodes) {
                 if(child instanceof Setting) {
-                    settings[child.key] = {
-                        default: child.default_val,
-                        on_change: child.on_change,
-                        saved_val: child.data.value
+                    if(child.key === undefined) // If setting is not initialized yet, don't save it
+                        continue
+                    if(Settings.saved[category] === undefined)
+                        Settings.saved[category] = {settings:{}}
+                    Settings.saved[category].settings[child.key] = {
+                            //default: child.default_val,
+                            //on_change: child.on_change,
+                            saved_val: child.data.value
                     }
                 }
             }
 
-            Settings.saved[category] = {
-                settings: settings
-            }
         }
         window.setCookie("settings", JSON.stringify(Settings.saved), 9999)
     },
@@ -165,6 +154,7 @@ window.Settings = {
         for(var category of Object.keys(Settings.saved)) {
             setting = Settings.saved[category].settings[setting_key] ?? setting
         }
+        
         return setting
     },
     set: function(setting_key, value) {
@@ -181,9 +171,9 @@ window.Settings = {
         return setting_val
     },
     set_saved_val: function(setting_key, value) {
-        for(var category of Settings.saved) {
-            if(category.settings[setting_key])
-                category.settings[setting_key].saved_val = value
+        for(var category of Object.keys(Settings.saved)) {
+            if(Settings.saved[category].settings[setting_key])
+                Settings.saved[category].settings[setting_key].saved_val = value
         }
     },
     add_category: function(category_key, label) {
@@ -207,7 +197,7 @@ window.Settings = {
     add_setting: function(category_key, key, component, label, default_val, on_change) {
         if(Settings.categories[category_key]) {
             let setting = document.createElement(component)
-            
+
             const loadingElement = $('#settings-loading')[0]
             loadingElement.style.display = ''
 
@@ -295,11 +285,12 @@ window.Setting = class Setting extends HTMLElement {
 
 
 
-// TODO: Implement hidden class filter setting
+// TODO: Implement hidden class/teacher filter setting
 
 
-
-// Legacy code
+/////////////////
+// Legacy code //
+/////////////////
 
 /**
 * globally reachable settings object
@@ -307,165 +298,6 @@ window.Setting = class Setting extends HTMLElement {
 window.settings = {
     filter: '',
     currentDateOffset: 0,
-    is_teacher: false,
-    darkmode: false,
-    autoscroll: false
 }
   
 const TIME_SHOW_NEXT_DAY = 16 // :00 o' clock in 24-hour format
-  
-
-
-// /**
-// * show settings menu
-// */
-// window.showSettings = function () {
-//     $('#settings-overlay-container')[0].style.visibility = 'visible'
-//     $('#settings-overlay-container')[0].style.opacity = 1
-//   }
-  
-//   /**
-//   * hide settings menu
-//   */
-//   window.hideSettings = function () {
-//     $('#settings-overlay-container')[0].style.visibility = 'hidden'
-//     $('#settings-overlay-container')[0].style.opacity = 0
-//   }
-  
-//   /**
-//   * applies url parameters to cookies
-//   */
-//   window.applyUrlParameters = function () {
-//     var urlParams = new URLSearchParams(window.location.search)
-  
-//     for (var pair of urlParams.entries()) {
-//       window.setCookie(pair[0].toLowerCase(), pair[1].toLowerCase().split('')[0], 9999)
-//     }
-//   }
-  
-//   /**
-//   * load and apply settings stored in cookies,
-//   * get darkmode preference from browser if no darkmode cookie available
-//   */
-//   window.initSettings = function () {
-//     window.applyUrlParameters()
-  
-//     if (window.getCookie('filter')) {
-//       window.settings.filter = window.getCookie('filter')
-//     } else {
-//       window.settings.filter = ""
-//     }
-  
-//     if (window.getCookie('darkmode')) {
-//       window.settings.darkmode = window.getCookie('darkmode') === 'true' // get darkmode preference from cookie
-//     } else {
-//       window.settings.darkmode = window.matchMedia('screen and (prefers-color-scheme: dark)').matches // get darkmode preference from browser
-//     }
-  
-//     // apply darkmode setting
-//     window.setDarkmode(window.settings.darkmode)
-  
-//     // apply autoscroll setting
-//     window.setAutoscroll(window.Settings.get("autoscroll"))
-  
-//     // add accent color selects to settings menu
-//     let c
-//     const colorsElem = $('#colors')[0]
-  
-//     for (c of COLORS) {
-//       const colorSelector = document.createElement('div')
-//       colorSelector.style.background = c
-//       colorSelector.id = 'color' + c
-//       colorSelector.classList.add('colorSelector')
-//       colorSelector.setAttribute('onclick', 'colorClick(this)')
-//       colorsElem.appendChild(colorSelector)
-//     }
-  
-//     // get accent color from cookie, generate dynamic dark accent color, pass colors to CSS
-//     if (window.getCookie('accentColor')) {
-//       const accentColor = window.getCookie('accentColor')
-//       const accentDark = window.shadeColor(accentColor, -20)
-  
-//       document.querySelector('body').style.setProperty('--accent', accentColor)
-//       document.querySelector('body').style.setProperty('--accent-dark', accentDark)
-  
-//       if (COLORS.includes(accentColor.toUpperCase())) {
-//         document.getElementById('color' + accentColor.toUpperCase()).classList.add('selected')
-//       }
-//     }
-  
-//     // get teacher mode preference from cookie
-//     if (window.getCookie('isTeacher')) {
-//       window.settings.isTeacher = window.getCookie('isTeacher') === 'true'
-//     }
-  
-//     // apply is teacher setting
-//     window.setIsTeacher(window.settings.isTeacher)
-//   }
-  
-//   window.setIsTeacher = function (isTeacher) {
-//     window.settings.isTeacher = isTeacher
-  
-//     $('#teacher-switch')[0].checked = window.settings.isTeacher
-//     window.setCookie('isTeacher', window.settings.isTeacher, 9999)
-  
-//     if (window.settings.isTeacher) {
-//       window.settings.selectedClass = ''
-//     } else {
-//       window.settings.selectedTeacher = ''
-//     }
-  
-//     if (window.settings.isTeacher) {
-//       $('.substitutions')[0].classList.add('teacher')
-//     } else {
-//       $('.substitutions')[0].classList.remove('teacher')
-//     }
-  
-//     window.draw()
-//   }
-//   /**
-//   * sets darkmode setting and applies it.
-//   * @param {boolean} darkmode value to apply to darkmode setting
-//   */
-//   window.setDarkmode = function (darkmode) {
-//       window.settings.darkmode = darkmode
-    
-//       $('#darkmode-switch')[0].checked = window.settings.darkmode
-//       window.setCookie('darkmode', window.settings.darkmode, 9999)
-    
-//       if (window.settings.darkmode) {
-//         document.body.classList.add('dark')
-//       } else {
-//         document.body.classList.remove('dark')
-//       }
-//     }
-  
-//   /**
-//   * handle onclick on accent color select element
-//   * @param {HTMLElement} obj clicked checkbox element
-//   */
-//   window.colorClick = function (obj) {
-//     Array.from(document.getElementsByClassName('colorSelector')).forEach((el) => {
-//       el.classList.remove('selected')
-//     })
-//     obj.classList.add('selected')
-  
-//     const accentColor = window.rgb2hex(obj.style.backgroundColor)
-//     const accentDark = window.shadeColor(accentColor, -20)
-  
-//     document.body.style.setProperty('--accent', accentColor)
-//     document.body.style.setProperty('--accent-dark', accentDark)
-  
-//     window.setCookie('accentColor', accentColor, 9999)
-//   }
-  
-//   /**
-//   * set or unset selected teacher / class
-//   *
-//   * @param {HTMLElement} elem selectedElement HTMLElement
-//   * @param {*} data API data of the current day
-//   */
-//   window.setSelectedElement = function (elem) {
-//     window.settings.filter = elem
-//     window.setCookie('filter', elem)
-//   }
